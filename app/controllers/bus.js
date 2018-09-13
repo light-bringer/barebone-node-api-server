8132fd908968a2c6962fb2f2d001b38e3e70117f
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-09-13 18:34:11 +0530

    added demo bus schema

---
+++ app/controllers/bus.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const Q = require('q')
+  , _ = require('lodash')
+  , config = require(__dirname + '/../../config')
+  , appDir = config.appDir
--- app/models/bus.js
+++ app/models/bus.js
@@ -1,5 +1,24 @@
 'use strict'
 
+/*
+*********** BUS SCHEMA ************
+{
+  '_id' : 1,
+  'number' : "KA 01 HK 1234",
+  'capacity' : 40,
+  'departs' : {
+    'city': "BLR",
+    'time': "19.45"
+  },
+  'arrives' : {
+    'city': "MYS",
+    'time': "21.45"
+  },
+  'booked' : 12
+}
+
+*/
+
 const Q = require('q')
   , _ = require('lodash')
   , config = require(__dirname + '/../../config')
